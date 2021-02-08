74f0e682d66c383073a9dbf8a2b424970267369b
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2021-02-08 17:03:22 +0900

    add

--- rooms/admin.py
+++ rooms/admin.py
@@ -11,7 +11,61 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost", 
+        "city", 
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "country", 
+        )
+
+    search_fields = ("=city", "^host__username",)
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+        )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+    
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
