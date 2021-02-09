8973129dc8a455cf49f92124abb46eb13baf21a6
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2021-02-09 15:59:32 +0900

    add

--- users/admin.py
+++ users/admin.py
@@ -19,7 +19,24 @@ class CustomUserAdmin(UserAdmin):
                     "langauge",
                     "currency",
                     "superhost",
-                    )
+                )
             },
         ),
-)
+    )  
+
+    list_filter = UserAdmin.list_filter + (
+        "superhost",
+    )
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "langauge",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
