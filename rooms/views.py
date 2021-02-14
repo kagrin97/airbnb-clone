930067b051c98c4b08362afd36f07340a9423cc5
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2021-02-14 14:15:15 +0900

    add

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
-from django.utils import timezone
 from django.views.generic import ListView
+from django.http import Http404
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 class HomeView(ListView):
@@ -12,8 +14,19 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )
