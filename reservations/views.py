d2577638baedecb409a58a8eda6450cb49325de7
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2021-03-06 14:35:20 +0900

    add

--- reservations/views.py
+++ reservations/views.py
@@ -4,9 +4,11 @@ from django.views.generic import View
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from rooms import models as room_models
+from reviews import forms as review_forms
 from . import models
 
 # Create your views here.
+
 
 class CreateError(Exception):
     pass
@@ -40,10 +42,13 @@ class ReservationDetailView(View):
             and reservation.room.host != self.request.user
         ):
             raise Http404()
+        form = review_forms.CreateReviewForm()
         return render(
-            self.request, "reservations/detail.html", {"reservation": reservation}
+            self.request,
+            "reservations/detail.html",
+            {"reservation": reservation, "form": form},
         )
-    
+
 
 def edit_reservation(request, pk, verb):
     reservation = models.Reservation.objects.get_or_none(pk=pk)
