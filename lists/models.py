a875b4c7296a0b8531694b6b0757a9222d6c0d9b
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2021-03-08 19:57:25 +0900

    add

--- lists/models.py
+++ lists/models.py
@@ -6,7 +6,7 @@ class List(core_models.TimeStampedModel):
     """  List Model Definiton """
 
     name = models.CharField(max_length=80)
-    user = models.OneToOneField("users.User", related_name="lists", on_delete=models.CASCADE)
+    user = models.OneToOneField("users.User", related_name="list", on_delete=models.CASCADE)
     room = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
 
     def __str__(self):
